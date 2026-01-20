00fc1ba5c2b461ffd62161135b8d8676ee86d537
khushikevadiya2222 <khushikevadiya2222@gmail.com>
2026-01-20 09:31:01 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,72 @@
+'''Python 3.13.11.(tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.'''
+
+'''print("wello,welcome to python programing:")
+a=int(input("enter first number:"))
+b=int(input ("enter second number:"))
+sum=a+b
+print("sum=",sum)
+================================='''
+'''num=int(input("enter a number:"))
+a=60
+if num%2==0:
+    print("the number is even")
+else:
+    print("the number is odd")
+==================================='''
+'''year=int(input("enter a year:"))
+if(year%400==0)or(year%4==0 and year %100 !=0):
+    print("leap year")
+else:
+    print("not a leap year")
+===================================='''
+'''import math
+print("PI value is:", math.pi)
+====================================='''
+'''PI=3.14
+print(PI)
+===================================='''
+'''num=int(input("enter a number:"))
+square=num*num
+print("square is:",square)
+================================='''
+'''r=float(input("enter radius:"))
+area=3.14*r*r
+print("area of circle is:",area)
+================================'''
+'''a=17
+b=25.10
+c="khushi"
+d=true
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+=============================='''
+'''import math
+print(math.sqrt(16))
+print(math.pow(2,3))
+print(math.ceil(4.2))
+print(math.fllo(4.8))
+=========================='''
+'''import math
+a=int(input("enter base:"))
+b=int(input("enter base:"))
+print(math.pow(a,b))
+==========================='''
+num=int(input("enter the number:"))
+if num>0:
+    print("positive")
+elif num<0:
+    print("nagative")
+else:
+    print("zero")
+
+
+
+
+
+    
+
+
