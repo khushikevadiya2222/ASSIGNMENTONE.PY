2bebe5a3889f0511513c55cca60202cfff92e0d8
khushikevadiya2222 <khushikevadiya2222@gmail.com>
2026-02-24 08:09:06 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,36 @@
+for i in range(1,6):
+    print(i)
+
+#2
+for i in range(3):
+    print("hello")
+#3
+for i in range(1,11):
+    print(i)
+
+#4
+for i in range(1,16):
+    if i%2==0:
+        print(i)
+#5
+for i in range(1,21):
+     if i%2==0:
+        print(i)
+
+#6
+for i in range(1,11):
+    print("5x",i,"=",5*1)
+
+#6
+name="atmiya"
+for letter in name:
+    print(letter)
+
+#7
+for i in range(1,16):
+    total=total+1
+    print("sum is:",total)
+#8
+numbers=[10,20,30,40]
+for n in numbers:
+    print(n)
