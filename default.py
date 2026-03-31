123d5bcd765701f0a96ca06d2d5132db897b121c
khushikevadiya2222 <khushikevadiya2222@gmail.com>
2026-03-31 07:43:47 +0530

    Add files via upload

---
+++ default.py
@@ -0,0 +1,15 @@
+#1basic default argument
+
+def greet(name,msg="good morning"):
+     print("hello", Name+",",msg)
+     greet("asha")
+     greet("ravi","good morning")
+
+
+#2
+def power(num,exp=2):
+ return num**exp 
+ print(power(3)) 
+ print(power(3,3))
+ print(power(2,4))
+
