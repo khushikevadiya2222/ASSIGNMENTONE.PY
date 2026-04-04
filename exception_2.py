fe0baef25c283363c86e8e4565975e2111e680ad
khushikevadiya2222 <khushikevadiya2222@gmail.com>
2026-04-04 09:01:39 +0530

    Add files via upload

---
+++ exception_2.py
@@ -0,0 +1,11 @@
+try:
+    my_list=[10,20,30,40,50]
+    print(my_list[1])
+    
+except IndexError:
+    print("Index is out of range")
+
+else:
+    print("element found successfully")
+finally:
+    print("program finished")
